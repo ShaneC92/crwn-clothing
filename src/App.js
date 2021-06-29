7107cffcac36ed06e5290acf9e11665ac12ea036
Shane Cox <S.Cox019@gmail.com>
2021-06-29 08:59:44 -0400

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import './pages/homepage/homepage.styles.scss';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route  exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
